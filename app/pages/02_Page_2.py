52f6833f0f7d24fda6ff9c3b9798a4afb5c82fde
YiWen Hon <yiwench@gmail.com>
2023-03-02 20:27:35 +0000

    added balloons

--- app/pages/02_Page_2.py
+++ app/pages/02_Page_2.py
@@ -6,6 +6,7 @@ today = str(date.today())
 name = st.text_input('What is your name?')
 bday = str(st.date_input('What is your birthday?'))
 if st.button('Submit'):
+    st.balloons()
     st.write(f'Why hello there {name}!')
     if bday[-5:] == today[-5:]:
         st.write('WOOOOOOO Happy birthday!')
